5a94a00460ff866669d30ca4c5d7323d78581997
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:41:45 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -19,6 +19,10 @@ $this->registerModule(
     '1.7',                                                   // Version
     [
         'requires' => [['core', '2.19']], // Dependencies
-        'type'     => 'theme'             // Type
+        'type'     => 'theme',            // Type
+
+        'details'    => 'https://open-time.net/?q=domino',       // Details URL
+        'support'    => 'https://github.com/franck-paul/domino', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/domino/main/dcstore.xml'
     ]
 );
